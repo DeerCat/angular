32f1cf413b0c9e73b81a59974220d7dac3c286b4
deercat <zyanlumia@hotmail.com>
2016-03-28 14:08:44 +0800

    l

---
+++ src/scripts/action/demo1.js
@@ -0,0 +1,5 @@
+var fnUtil = require('../util/fn-util.js');
+var strDemo1 = require('../tpl/demo1.string');
+fnUtil.render({
+	str: strDemo1
+});
---
+++ src/scripts/course-angular.js
@@ -0,0 +1,3 @@
+require('Angular/angular.min.js');
+
+require('./action/demo1.js');
